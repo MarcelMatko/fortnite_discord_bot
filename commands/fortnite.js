20168f537727256837ed7b9827f0aa69d745bd92
OfficialFalcco <matko518@gmail.com>
2018-07-06 17:31:13 +0200

    1234
    0089

---
+++ commands/fortnite.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+const config = require("../botconfig.json");
+const apikey = require("../keys.json");
+
+exports.run = async (client, message, args) => {
+  let username = args[0];
+  let platform = args[1] || "pc";
+
+  if (!username) return message.reply('You must supply a username');
+  
+  const Fortnite = require('fortnite');
+  const fortnite = new Fortnite('81e84f99-070d-4343-ac2a-ff72f7fc380e')
+
+ fortnite.user(username, platform).then(data => {
+
+    let kills = data.stats.lifetime[10]['Kills']
+    let score = data.stats.lifetime[6]['Score']
+    let matchesPlayed = data.stats.lifetime[7]['Matches Played']
+    let wins = data.stats.lifetime[8]['Wins']
+    let winPercent = data.stats.lifetime[9]['Win%']
+    let KD = data.stats.lifetime[11]['K/d']
+    let Top5s = data.stats.lifetime[1]['Top 5s']
+
+    const embed = new Discord.RichEmbed()
+    .setAuthor(`${data.username}'s Fortnite stats on ${data.platform}`)
+    .setThumbnail('https://cdn2.unrealengine.com/Fortnite%2Fsearch-for-survivors%2FsignupBanner-155x221-7d1f31411baf91e6cadf490c6f60f98a72b38b4c.png')
+    .setColor('RANDOM')
+    .addField('Kills', kills, true)
+    .addField('Score', score, true)
+    .addField('Wins', `${wins} (${winPercent})`, true)
+    .addField('K/D Ratio', KD, true)
+    .addField('Top 5s', Top5s, true)
+    .addField('Matches Played', matchesPlayed, true)
+    .setFooter(`Requested by: ${message.author.username} `, message.author.avatarURL)
+    .setTimestamp()
+
+    message.channel.send(embed);
+
+
+    }).catch(e => {
+        console.log(e)
+        return message.reply('Invalid Search - An error occured');
+    });
+};
+
+module.exports.help = {
+  name: "stats"
+}
---
+++ commands/shop (Marcel Matko's conflicted copy).js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const request = require('request');
+
+// Your token for fnbr - https://fnbr.co/api/docs
+const fnbr_token = '7bfd6b4e-dcd4-4607-b99c-9a636aae9f28';
+
+exports.run = async (client, message, args) =>{ 
+    var options = {
+        method: "GET",
+        url: "https://fnbr.co/api/shop",
+        headers: {
+          'User-Agent': 'nodejs request',
+          'x-api-key': fnbr_token
+        }
+    }
+
+    request(options, function(error, response, body) {
+        if (error) return;
+        var responsedata = JSON.parse(body);
+
+        data = responsedata["data"]
+        daily = data["daily"]
+        featured = data["featured"]
+        date = data["date"]
+
+        var item, i, embed;
+        for (i=0;i<featured.length;i++) {
+            item = featured[i];
+            embed = new Discord.RichEmbed();
+            embed.setColor('RED')
+            embed.addField('Featured Item: ' + item.name, 'Price: ' + item.price + ' VBucks');
+            embed.setThumbnail(item.images.icon);
+            embed.setFooter(`Visit us on: myfortnitestats.tk ` + ' | API by: fnbr.co')
+            message.channel.send(embed);
+        }
+        for (i=0;i<daily.length;i++) {
+            item = daily[i];
+            embed = new Discord.RichEmbed();
+            embed.setColor('GREEN')
+            embed.addField('Daily Item: ' + item.name, 'Price: ' + item.price + ' VBucks');
+            embed.setThumbnail(item.images.icon);
+            embed.setFooter(`Website: myfortnitestats.tk ` + ' | API by: fnbr.co')
+            message.channel.send(embed);
+        }
+    })
+};
+
+module.exports.help = {
+  name: "shop"
+}
+
